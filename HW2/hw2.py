095610df2eab118f2572131ba108f7366892a2b2
Himaja <himaja.auce@gmail.com>
2016-02-13 21:52:21 -0800

    homotopy implemented

--- HW2/hw2.py
+++ HW2/hw2.py
@@ -1,9 +1,12 @@
 import numpy as np
 from scipy.optimize import minimize
 from sklearn.cross_validation import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import Ridge
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import time
 
 
 def ridge(X,y,theta,Lambda):
@@ -15,7 +18,8 @@ def compute_loss(X,y,theta):
 	return ((np.linalg.norm(np.dot(X,theta) - y))**2)/(2*N)
 
 def coordinate_descent(X,y,lamda,w):
-	k = 0	
+	k = 0
+	print 'lamda in coordinte descent function  ' , lamda	
 	while(k <= 10):
 		for j in range(D):
 			xij_factor = 0
@@ -81,18 +85,39 @@ def main():
 	plt.savefig('Ridge_solution.png')	
 	plt.close()	
 	
-	#Report after choosing a small threshold of 10**-3 and making all coefficients smaller than the threshold to zero
+	# Comparing with sklearn Ridge
 
-	theta_learned[theta_learned < 10**-3] = 0
+	clf = Ridge(alpha=2*N*least_loss_lambda)
+	clf.fit(X_train, y_train)
+	sklearn_ridge_loss = mean_squared_error(clf.predict(X_validation), y_validation) / 2
+	print 'sklearn Ridge Regression loss is  ' , sklearn_ridge_loss
+	sklearn_ridge_coef = clf.coef_
+
+	# plot for comparison between Ridge regression and sklearn ridge solution 
+
 	plt.ylim(-1,1)
-	plt.plot(theta_learned,'r',label = "ridge solution scaled")
+	plt.plot(theta_learned,'r',label = "ridge solution")
+	plt.plot(sklearn_ridge_coef,'g',label="sklearn solution")
+	legend = plt.legend(loc='upper center', shadow=True, fontsize='large')
+    	legend.get_frame().set_facecolor('#00FFCC')
+	plt.title('Ridge solution Vs sklearn Ridge solution')
+	plt.savefig('sklearn_vs_ridge.png')	
+	plt.close()	
+
+
+	#Report after choosing a small threshold of 10**-3 and making all coefficients smaller than the threshold to zero
+	theta_scaled = theta_learned
+	theta_scaled[theta_scaled < 10**-3] = 0
+	plt.ylim(-1,1)
+	plt.plot(theta_scaled,'r',label = "ridge solution scaled")
 	plt.plot(true_theta,'g',label="true solution")
 	legend = plt.legend(loc='upper center', shadow=True, fontsize='large')
     	legend.get_frame().set_facecolor('#00FFCC')
 	plt.title('Ridge scaled solution Vs True solution')
 	plt.savefig('Ridge_scaled_solution.png')	
 	plt.close()	
-	
+
+
 	###########################################################################
 	
 	## 2.1 Coordinate descent Function for 10 diffenent lambdas ranging from 10 ** -5 to 10**6
@@ -100,10 +125,10 @@ def main():
 	sq_loss_val_set = np.zeros(11)
 	sq_loss_train = np.zeros(11)
 	lamda_range = np.zeros(11)
-	for i in range(-5,6):
+	for i in range(-7,8):
 		lamda = 10**i
 		lamda_range[i] = lamda
-		w_starting_point = np.zeros(D) #np.dot(np.dot(np.linalg.inv(np.dot(X_train.T,X_train) + lamda * np.identity(X_train.shape[1])),X.T),y_train)
+		w_starting_point =  theta_learned#np.dot(np.dot(np.linalg.inv(np.dot(X_train.T,X_train) + lamda * np.identity(X_train.shape[1])),X_train.T),y_train)#np.zeros(D)
 		lasso_solutions[i] = coordinate_descent(X_train,y_train,lamda,w_starting_point)
 		sq_loss_val_set[i] = compute_loss(X_validation,y_validation,lasso_solutions[i])
 		sq_loss_train[i] = compute_loss(X_train,y_train,lasso_solutions[i])
@@ -113,7 +138,7 @@ def main():
 	test_error = compute_loss(X_test,y_test,test_lasso_sol)
 	print 'lamda which minimizes square  loss on validation set is ' , best_lamda
 	print 'The corresponding test error is ' , test_error
-	plt.plot(np.log(lamda_range),sq_loss_val_set,'g',label = "Validation error Vs lamda")
+	plt.plot(np.log(lamda_range),np.log(sq_loss_val_set),'g',label = "Validation error Vs lamda")
 	plt.title('Validation square loss Vs lambda')
 	plt.xlabel('lamda')
 	plt.ylabel('square_loss')
@@ -121,7 +146,7 @@ def main():
 	print sq_loss_val_set		 
 	print sq_loss_train
 	plt.close()
-	##############################################################################
+	####################################################################################################################################################
 
 	## 2.2 Analyze the sparsity of solution
 	print lasso_solutions[np.argmin(sq_loss_val_set)]
@@ -132,7 +157,51 @@ def main():
     	legend.get_frame().set_facecolor('#00FFCC')
 	plt.title('sparsity in lasso solution Vs True solution')
 	plt.savefig('sparsity.png')	
+	plt.close()
+	######################################################################################################################################################	
+
+	## 2.3 homotopy Method Vs Basic Shooting Algorithm
+	lamda_max = 2 * np.linalg.norm(np.dot(X_train.T,y_train))
+	i = 0
+	w_starting_point = np.zeros(D)
+	lamda_len = 0
+	lamda = lamda_max
+	while(lamda >= 10 ** -5):
+		lamda_len = lamda_len + 1
+		lamda = lamda / 2
+	w = np.zeros((lamda_len,D))
 	
+	#Run time for Basic Shooting Algorithm
+	w_starting_point = np.zeros(D)
+	i = 0
+	lamda = lamda_max
+	start_time = time.clock()
+	shooting_time = 0
+	while(lamda >= 10 ** -5):
+		start_time = time.clock()
+		w[i] = coordinate_descent(X_train,y_train,lamda,w_starting_point)
+		end_time = time.clock()
+
+		shooting_time = shooting_time + (end_time - start_time)
+
+		lamda = float(lamda) / 10
+		i = i + 1
+	print 'Time taken by shooting algorithm  ', shooting_time
+	#Run-time for homotopy method
+	lamda = lamda_max
+	homotopy_time = 0
+	while(lamda >= 10 ** -5):
+		start_time = time.clock()
+		w[i] = coordinate_descent(X_train,y_train,lamda,w_starting_point)
+		end_time = time.clock()
+		
+		homotopy_time = homotopy_time + (end_time - start_time)
+		w_starting_point = w[i]
+		lamda = float(lamda) / 10
+		i = i + 1
+
+	print 'Time taken by homotopy method is  ' , homotopy_time
+
 	
 if __name__ == "__main__":
     main()
